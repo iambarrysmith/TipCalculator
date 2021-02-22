f3b613a8700035c5535a5723f34dac9a8482012f
iambarrysmith <78829890+iambarrysmith@users.noreply.github.com>
2021-02-21 19:13:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ var num1 = Number(prompt("What is your bill?"));
 var num2 = (num1*0.07)+num1;
 var num3 = num2*0.05;
 var total = num2+num3;
-containerEle.innerHTML = "Your final bill is "+total+"."
+containerEle.innerHTML = "Your final bill is $"+total+"."
